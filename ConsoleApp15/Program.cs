4502ac6a2080907a1aa41a4c360eb2519dc5fd51
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2025-04-24 10:47:08 +0300

    Add files via upload

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        delegate void Message(); // Объявление делегата
+
+        static void Main(string[] args)
+        {
+            Message message; // Создание объекта делегата
+
+            DateTime now = DateTime.Now;
+            int hour = now.Hour;
+
+            if (hour < 12)
+            {
+                message = GoodMorning;
+            }
+            else if (hour < 18)
+            {
+                message = GoodAfternoon;
+            }
+            else
+            {
+                message = GoodEvening;
+            }
+
+            message(); 
+            Console.ReadKey();
+        }
+
+        static public void GoodMorning() => Console.WriteLine("Доброе утро");
+        static public void GoodAfternoon() => Console.WriteLine("Добрый день");
+        static public void GoodEvening() => Console.WriteLine("Добрый вечер");
+
+    }
+}
